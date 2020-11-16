e7a2dbf2bf22490939921f6515f14dafa76f2f80
Mayur_Jain <mayur87545@gmail.com>
2020-11-16 20:15:31 +0530

    Pytorch N2D

--- n2d.py
+++ n2d.py
@@ -336,7 +336,7 @@ if __name__ == "__main__":
 
     label_names = None
     transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
-    trainset = torchvision.datasets.MNIST(root='/home/mayur/Desktop/Pytorch/data', train=True, download=False, transform=transform)
+    trainset = torchvision.datasets.MNIST(root='../data', train=True, download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True, num_workers=16)
     device = (torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu'))
 
